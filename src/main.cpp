76f36d5a1388ed853afbadb098b652c34197cdcd
cawl0909 <117081278+cawl0909@users.noreply.github.com>
2026-01-04 13:03:29 +0000

    currently working

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 #include "vectors.h"
 
+const int WINDOW_SIZE_X = 320;
+const int WINDOW_SIZE_Y = 240;
+const char WINDOW_NAME[] = "CFD SIM";
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout<<"First cfd test"<<std::endl;
-    std::cout<<"Hello world, first cfd test"<<std::endl;
-    vector2d temp{1,2};
-    std::cout<<temp<<std::endl;
-    std::cout<<"Happy as can be"<<std::endl;
+    if(!(SDL_Init(SDL_INIT_VIDEO)))
+    {
+        SDL_Log("Failed to start");
+        std::cerr<<"SDL failed to init video"<<"\n";
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL3",800, 600,0);
+
+    bool running = true;
+    while (running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT)
+                running = false;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
