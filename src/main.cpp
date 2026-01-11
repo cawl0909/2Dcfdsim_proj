62ab03d442bfc257fe73f0363697a541ac8d97b4
cawl0909 <117081278+cawl0909@users.noreply.github.com>
2026-01-11 18:19:04 +0000

    fixed imgui cmake setup

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
+
 #include "vectors.h"
 #include "Fluid.h"
 
